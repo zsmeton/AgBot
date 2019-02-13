261a1ea5343f6a9041bc0401e683ec82cea61e54
Kevin Barnard <kbarnard@mbari.org>
2019-02-12 17:06:50 -0700

    Completed collision_detector.py

--- src/navigation/scripts/collision_detector.py
+++ src/navigation/scripts/collision_detector.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
+import std_msgs.msg as msg
 
 # [collision_detector] uses (bumper_button) to detect when 
 # button is activated. If bumper is activated robot stops, turn 
@@ -9,31 +9,36 @@ from std_msgs.msg import String
 # true. Others can find what the robot state is through 
 # (collision_state)
 
-# This node is a TEMPLATE
+collision_state = False
 
 def bump_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + " Received bumper state: %s", data.data)
+    if (data.data):
+        global collision_state
+        collision_state = True  # Activate collision state 
 
 def safety_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + " Received safety button press.")
+    if (data.data):
+        global collision_state
+        collision_state = False  # Deactivate collision state
 
 def collision_detector():
     # Publisher to topic collision_state
-    pub_collision = rospy.Publisher('collision_state', String, queue_size=10)
+    pub_collision = rospy.Publisher('collision_state', msg.Bool, queue_size=10)
 
     # Subscribes to topic bumper_button
-    sub_bump = rospy.Subscriber('bumper_button', String, bump_callback)
+    sub_bump = rospy.Subscriber('bumper_button', msg.Bool, bump_callback)
 
     # Subscibes to topic safety_button
-    sub_safety = rospy.Subscriber('safety_button', String, safety_callback)
+    sub_safety = rospy.Subscriber('safety_button', msg.Bool, safety_callback)
 
     rospy.init_node('collision_detector')
     rate = rospy.Rate(1) # 1hz
     
     while not rospy.is_shutdown():
-        # Publishes "TEST" to course_correct
-        message = "TEST"
-        pub_collision.publish(message)
+        # Publishes collision state var to collision_state
+        pub_collision.publish(collision_state)
         #rospy.spin_once()
         rate.sleep()
         
